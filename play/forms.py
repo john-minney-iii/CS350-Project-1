baa0e602dd0833be007be24ade3f736f186c8422
john-minney-iii <jminneyiii@gmail.com>
2021-09-25 22:38:34 -0600

    Updated Play View template and form

---
+++ play/forms.py
@@ -0,0 +1,11 @@
+# CS350-Project-1/play/forms.py
+
+from django import forms
+
+class CardRateForm(forms.Form):
+    # Generate pairs such as ('1', 1)
+    CHOICES = [(str(x), x) for x in range(1, 11)]
+    card_rating = forms.CharField(
+        label='Rate the White Card from 1-10',
+        widget=forms.RadioSelect(choices=CHOICES)
+    )
--- play/views.py
+++ play/views.py
@@ -1,7 +1,21 @@
 # CS350-Project-1/play/views.py
 
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views import View
+from .forms import CardRateForm
 
-class PlayTemplateView(TemplateView):
+class PlayView(View):
     template_name = 'play.html'
+    
+    def get(self, request):
+        context = {'form': CardRateForm()}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = CardRateForm(request.POST)
+        if form.is_valid():
+            card_rating = form.cleaned_data['card_rating']
+            print('Card Rating')
+            # Redirect back to Play
+            return HttpResponseRedirect('/play/')

0670f4220a22176e68a9d56506dfd312b0d9582c
OWamsley <47764303+OWamsley@users.noreply.github.com>
2021-09-29 14:27:40 -0600

    Added Score Model (#7)
    * Added Score Model
    Edited WhiteCard+ BlackCard models
    Filled Database with 50 whitecards, 10 blackcards
    
    Co-authored-by: John Minney III <jminneyiii@gmail.com>

--- play/models.py
+++ play/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 
-class BlackCard(models.Model):
-    card_text = models.CharField(max_length=100)
-
-    def __str__(self) -> str:
-        return self.card_text
 
 class WhiteCard(models.Model):
-    card_text = models.CharField(max_length=100)
-    average_rank = models.DecimalField(max_digits=4, decimal_places=2, default=0.00)
+    CardNo = models.IntegerField()
+    CardText = models.CharField(max_length=50)
 
-    def __str__(self) -> str:
-        return self.card_text
+class BlackCard(models.Model):
+    CardNo = models.IntegerField()
+    CardText = models.CharField(max_length=50)
+
+
+class Score(models.Model):
+    #Uses two foreign keys to define the relationship
+    WhiteCard = models.ForeignKey(WhiteCard, models.CASCADE)
+    BlackCard = models.ForeignKey(BlackCard, models.CASCADE)
+    rating = models.DecimalField(max_digits=4,decimal_places=2)
+    timesAppeared = models.IntegerField(default=0)

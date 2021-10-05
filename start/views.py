2d4b7231888c999524f0466229a26deec8cbb9cd
Niko-Antuna <70356132+Niko-Antuna@users.noreply.github.com>
2021-10-05 16:47:56 -0600

    Random Text Added

--- start/views.py
+++ start/views.py
@@ -1,7 +1,36 @@
 # CS350-Project-1/start/views.py
 
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views import View
+from play.models import BlackCard, WhiteCard
+from play.models import WhiteCard
 
-class StartTemplateView(TemplateView):
+import random
+
+
+class StartTemplateView(View):
     template_name = 'index.html'
+
+    def get(self, request):
+        context = {'black_text': self.getRandomBlackCardText,
+                   'white_text': self.getRandomWhiteCardsText}
+        return render(request, self.template_name, context)
+
+    """
+        Get a Random Black card in the Database
+        This might need to change depending on what the backend team does
+        @return Black Card
+    """
+
+    def getRandomBlackCardText(self) -> str:
+        return random.choice(list(BlackCard.objects.all())).CardText
+
+    """
+        Get a Set of Random White Cards from the Database
+        This might need to change depending on what the backend team does
+        @param set_size (int): Number of white cards to return
+        @return List of White Cards
+    """
+
+    def getRandomWhiteCardsText(self) -> str:
+        return random.choice(list(WhiteCard.objects.all())).CardText

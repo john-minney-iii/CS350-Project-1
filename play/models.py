62b57fb062c06806b035890213b8f75c0f19f4f5
Justin Moehlenpah <moeh9589@bears.unco.edu>
2021-10-06 15:31:28 -0600

    Update models.py

--- play/models.py
+++ play/models.py
@@ -8,6 +8,14 @@ class WhiteCard(models.Model):
     def __str__(self) -> str:
         return self.CardText
 
+
+    def findScore(self, blackCard):
+        for score in Score.objects.all():
+            if score.Whitecard == self:
+                if score.Blackcard == self:
+                    return score
+
+
 class BlackCard(models.Model):
     CardNo = models.IntegerField()
     CardText = models.CharField(max_length=50)
@@ -16,9 +24,24 @@ class BlackCard(models.Model):
         return self.CardText
 
 
+    def findScore(self, whiteCard):
+        for score in Score.objects.all():
+            if score.Blackcard == self:
+                if score.Whitecard == self:
+                    return score
+
+
 class Score(models.Model):
-    #Uses two foreign keys to define the relationship
+    # Uses two foreign keys to define the relationship
     WhiteCard = models.ForeignKey(WhiteCard, models.CASCADE)
     BlackCard = models.ForeignKey(BlackCard, models.CASCADE)
     rating = models.DecimalField(max_digits=4,decimal_places=2)
     timesAppeared = models.IntegerField(default=0)
+
+    def return_averages(self):
+        average = self.rating / self.timesAppeared
+        return average
+
+    def add_score(self, score):
+        self.rating += score
+        self.timesAppeared += 1

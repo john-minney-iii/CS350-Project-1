fbcca587ca2634ef2ee92f9dcab75ac9f3394178
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-10-07 18:27:14 -0600

    Rework Form UI (#8)
    * demo commit
    
    * undo demo commit
    
    * change form radio buttons to dropdown menu
    
    * remove form label and change rating range to 0-10 to match wireframe
    
    * add new form label to match wireframe
    
    * switch classes to match wireframe
    
    * change line ending
    
    * add center style rule and adjust form style to be centered and larger
    
    * adjust text sizes
    
    * format code
    
    * add buttons to form and button style rules
    
    * replace old form with button template
    
    * add placeholder button logic
    
    * remove duplicate method
    
    * remove submit and quit button
    
    * remove unnecessary break
    
    * add vertical center style rules and center white card text
    
    * vertically center top display card text
    
    * add true center style rule and card numbers. also apply center style rules to card text
    
    * add placeholders to display the card's average rating
    
    * update play with a quit button
    
    need to add href to link play page to end page
    
    * added href for quit button
    
    * added some style to rating buttons
    
    added margin to gave buttons some space (margin) and made the buttons turn dark gray when the cursor is hovering over a rating button
    
    Co-authored-by: Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>

--- play/forms.py
+++ play/forms.py
@@ -4,8 +4,11 @@ from django import forms
 
 class CardRateForm(forms.Form):
     # Generate pairs such as ('1', 1)
-    CHOICES = [(str(x), x) for x in range(1, 11)]
-    card_rating = forms.CharField(
-        label='Rate the White Card from 1-10',
-        widget=forms.RadioSelect(choices=CHOICES)
-    )
+    # CHOICES = [(str(x), x) for x in range(0, 11)]
+    # card_rating = forms.CharField(
+    #     label='',
+    #     widget=forms.Select(choices=CHOICES)
+    # )
+
+    card_rating = forms.CharField()
+    

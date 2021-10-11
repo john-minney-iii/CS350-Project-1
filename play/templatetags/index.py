6e730c9bd4495721bf4b4aa635a740b35a639c11
John Minney III <jminneyiii@gmail.com>
2021-10-11 15:40:34 -0600

    Added Basic Communication with Backend (#12)

--- play/templatetags/index.py
+++ play/templatetags/index.py
@@ -1,5 +1,7 @@
 
 from django import template
+from play.models import WhiteCard, BlackCard, Score
+
 register = template.Library()
 
 """
@@ -9,3 +11,16 @@ register = template.Library()
 @register.filter
 def index(indexable, i):
     return indexable[i-1]
+
+@register.filter
+def getPK(indexable, i):
+    return indexable[i-1].pk
+
+"""
+    Get the Average Rating for a Black Card and White Card
+"""
+@register.simple_tag
+def get_score_average(obj, black_card):
+    print('White Card:', obj)
+    print('Black Card:', black_card)
+    return obj.findScoreAverage(black_card)

973749b6214ca0df217a28c5f0c1744fa19738e1
john-minney-iii <jminneyiii@gmail.com>
2021-10-12 16:23:37 -0600

    Django Static

--- config/settings.py
+++ config/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = str(BASE_DIR.joinpath('static'))
 STATICFILES_DIR = [str(BASE_DIR.joinpath('static'))]
 
 # Default primary key field type

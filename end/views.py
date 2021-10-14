6e854e88343b7f2a4df4ed06b7173c40bb4a34a9
john-minney-iii <jminneyiii@gmail.com>
2021-10-14 14:14:17 -0600

    Added Queries

--- end/views.py
+++ end/views.py
@@ -1,8 +1,10 @@
 # CS350-Project-1/end/views.py
 
+from typing import _SpecialForm
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views import View
+from django.db.models import Max
 
 from play.models import BlackCard, WhiteCard, Score
 from play.models import WhiteCard
@@ -14,6 +16,15 @@ class EndTemplateView(View):
 
         dataBlkCards = BlackCard.objects.all()
         dataWhiteCards = WhiteCard.objects.all()
+
+        highest_scores = []
+
+        for card in dataBlkCards:
+            score = Score.objects.filter(BlackCard=card).order_by('rating')[0]
+            highest_scores.append((card, score.WhiteCard))
+
         context = {'black_cards': dataBlkCards,
-                   'white_cards': dataWhiteCards}
+                   'white_cards': dataWhiteCards,
+                   'highest_scores': highest_scores
+                   }
         return render(request, self.template_name, context)
